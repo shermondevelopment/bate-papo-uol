3cf6b39fdcb566d68ff35c95db151b0c9eea49b1
Victor Shermon <victor804.gt@gmail.com>
2022-02-09 21:09:25 -0300

    making a request to the api to add a user to the chat

--- app/_js/main.js
+++ app/_js/main.js
@@ -3,3 +3,24 @@ function checkUser(){
     window.location.pathname = "/home.html"
   }
 }
+
+async function postRequest(router, data) {
+  const registerName = await fetch(`https://mock-api.driven.com.br/api/v4/uol/${router}`, {
+    method:'post',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({name: data})
+  })
+  return registerName;
+}
+
+async function sendForm(event) {
+  event.preventDefault()
+  const name = document.querySelector('input').value
+  const { status } = await postRequest('participants', name)
+  if(status === 200) {
+   localStorage.setItem('user', 'user')
+   window.location.href= "/"
+  }
+}
